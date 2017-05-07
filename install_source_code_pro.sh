e9f68802bce7a5255aa8a16a397050622fe637bc
Joseph Glanville <jpg@jpg.id.au>
2017-05-07 18:37:25 +1000

    Don't be so noisy when installing Source Code Pro
    Signed-off-by: Joseph Glanville <jpg@jpg.id.au>

--- install_source_code_pro.sh
+++ install_source_code_pro.sh
@@ -17,8 +17,8 @@ cleanup() {
 cleanup
 
 # Download, extract & set perms
-curl -fLs https://github.com/downloads/adobe/source-code-pro/$FILENAME -o $FILENAME
-unzip -u $FILENAME
+curl -fLso $FILENAME https://github.com/downloads/adobe/source-code-pro/$FILENAME
+unzip -qqu $FILENAME
 chmod 644 $DIR/OTF/*.otf
 
 # Install
